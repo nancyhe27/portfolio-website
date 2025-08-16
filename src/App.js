ea05158d9c29d6be3d283ba8c830171793686ab1
Nancy He <nancyhe@MacBook-Pro-81.local>
2025-08-16 16:18:15 -0500

    deploying 2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Project06 from './pages/projects/Project06';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio-website">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
