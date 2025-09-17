114996af2f75ce12b1f6523d14f04286f24ce22e
Nancy He <nancyhe@MacBook-Pro-81.local>
2025-09-17 00:14:59 -0500

    deploy attempt image

--- src/data/projects/project-01-data.js
+++ src/data/projects/project-01-data.js
@@ -179,7 +179,7 @@ export const project01Data = {
           ],
           "images": [
             {
-              "src": "/images/projects/project-01/design_system/upload.png",
+              "src": "/images/projects/project-01/design_system/Upload.png",
               "alt": "WeKruit Upload Interface",
               "caption": "WeKruit Upload Interface"
             }
